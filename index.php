94f4e406c74007966b1b4443bbbc62f82f2cc4a0
OgataiKhan <badellino.kevin@gmail.com>
2024-01-09 19:08:03 +0100

    feat: basic table style

--- index.php
+++ index.php
@@ -48,12 +48,15 @@ $hotels = [
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <!-- /Bootstrap -->
+  <!-- My css -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- /My css -->
 </head>
 
 <body>
-  <main>
-    <h1>Hotels</h1>
-    <table>
+  <main class="container">
+    <h1 class="text-center">Hotels</h1>
+    <table class="table">
       <tr>
         <th>Name</th>
         <th>Description</th>
